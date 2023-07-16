0b90f0ec8673fd32d21ebc769c8596c537c30f1a
Ali Sobhani <acdota@outlook.com>
2023-07-17 02:29:04 +0330

    editing Models

--- EFGetStarted/Models/Post.cs
+++ EFGetStarted/Models/Post.cs
@@ -10,5 +10,10 @@
 
 public class Post
 {
+    public int PostId { get; set; }
+    public string Title { get; set; }
+    public string Content { get; set; }
+    public int BlogId { get; set; }
+    public Blog Blog { get; set; }
 
 }
